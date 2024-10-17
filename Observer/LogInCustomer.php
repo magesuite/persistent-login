047863a0ea3fc742b558971ebe64706d8df545a3
Oleksandr Ivasiunko <olivas@smile.fr>
2024-10-17 08:40:26 +0000

    fix: [KRG-2898] redesign LoginCustomer observer to work correctly with...

--- Observer/LogInCustomer.php
+++ Observer/LogInCustomer.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MageSuite\PersistentLogin\Observer;
 
 class LogInCustomer implements \Magento\Framework\Event\ObserverInterface
 {
-    const CUSTOMER_WAS_LOGGED_DURING_CURRENT_REQUEST_KEY = 'CUSTOMER_LOGGED_IN_FROM_PERSISTENT';
+    public const CUSTOMER_WAS_LOGGED_DURING_CURRENT_REQUEST_KEY = 'CUSTOMER_LOGGED_IN_FROM_PERSISTENT';
 
     protected \Magento\Persistent\Helper\Session $persistentSession;
     protected \Magento\Persistent\Helper\Data $persistentData;
@@ -13,6 +15,7 @@ class LogInCustomer implements \Magento\Framework\Event\ObserverInterface
     protected \Psr\Log\LoggerInterface $logger;
     protected \Magento\Framework\Registry $registry;
     protected \Magento\Framework\App\RequestInterface $request;
+    protected \Magento\Framework\App\ResponseInterface $response;
     protected \Magento\Framework\App\ActionFlag $actionFlag;
 
     public function __construct(
@@ -22,6 +25,7 @@ class LogInCustomer implements \Magento\Framework\Event\ObserverInterface
         \Magento\Framework\App\Response\RedirectInterface $redirect,
         \Psr\Log\LoggerInterface $logger,
         \Magento\Framework\App\RequestInterface $request,
+        \Magento\Framework\App\ResponseInterface $response,
         \Magento\Framework\Registry $registry,
         \Magento\Framework\App\ActionFlag $actionFlag
     ) {
@@ -32,11 +36,16 @@ class LogInCustomer implements \Magento\Framework\Event\ObserverInterface
         $this->logger = $logger;
         $this->registry = $registry;
         $this->request = $request;
+        $this->response = $response;
         $this->actionFlag = $actionFlag;
     }
 
-    public function execute(\Magento\Framework\Event\Observer $observer)
+    public function execute(\Magento\Framework\Event\Observer $observer): void
     {
+        if ($this->request->isAjax()) {
+            return;
+        }
+
         if (!$this->persistentData->isEnabled()) {
             return;
         }
@@ -57,16 +66,9 @@ class LogInCustomer implements \Magento\Framework\Event\ObserverInterface
 
         try {
             $this->customerSession->loginById($customerId);
-
             $this->registry->register(self::CUSTOMER_WAS_LOGGED_DURING_CURRENT_REQUEST_KEY, true);
-
-            if (!$this->request->isAjax()) {
-                $this->actionFlag->set('', \Magento\Framework\App\ActionInterface::FLAG_NO_DISPATCH, true);
-
-                $controller = $observer->getControllerAction();
-                $request = $controller->getRequest();
-                $this->redirect->redirect($controller->getResponse(), $request->getUriString());
-            }
+            $this->actionFlag->set('', \Magento\Framework\App\ActionInterface::FLAG_NO_DISPATCH, true);
+            $this->redirect->redirect($this->response, $this->request->getUriString());
         } catch (\Exception $e) {
             $this->logger->error(sprintf('Logging in persistent customer failed, reason: %s', $e->getMessage()));
         }
