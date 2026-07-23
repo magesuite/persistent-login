a1b852edd217a3493df99af5f0efd4a390f0d0a6
Paweł Złamaniec <pawel.zlamaniec@creativestyle.pl>
2026-07-23 05:59:50 +0000

    [KRG-3531] Fix/krg 3531/missing get request

--- Observer/LogOutCustomerWhenTokenDoesNotMatch.php
+++ Observer/LogOutCustomerWhenTokenDoesNotMatch.php
@@ -1,15 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MageSuite\PersistentLogin\Observer;
 
 class LogOutCustomerWhenTokenDoesNotMatch implements \Magento\Framework\Event\ObserverInterface
 {
-    const CUSTOMER_WAS_LOGGED_DURING_CURRENT_REQUEST_KEY = 'CUSTOMER_LOGGED_IN_FROM_PERSISTENT';
+    public const CUSTOMER_WAS_LOGGED_DURING_CURRENT_REQUEST_KEY = 'CUSTOMER_LOGGED_IN_FROM_PERSISTENT';
 
     protected \Magento\Persistent\Helper\Session $persistentSession;
     protected \Magento\Persistent\Helper\Data $persistentData;
     protected \Magento\Customer\Model\Session $customerSession;
     protected \Magento\Framework\App\Response\RedirectInterface $redirect;
+    protected \Magento\Framework\App\ResponseInterface $response;
     protected \Psr\Log\LoggerInterface $logger;
     protected \Magento\Framework\App\RequestInterface $request;
     protected \Magento\Framework\App\ActionFlag $actionFlag;
@@ -21,6 +24,7 @@ class LogOutCustomerWhenTokenDoesNotMatch implements \Magento\Framework\Event\Ob
         \Magento\Persistent\Helper\Data $persistentData,
         \Magento\Customer\Model\Session $customerSession,
         \Magento\Framework\App\Response\RedirectInterface $redirect,
+        \Magento\Framework\App\ResponseInterface $response,
         \Psr\Log\LoggerInterface $logger,
         \Magento\Framework\App\RequestInterface $request,
         \Magento\Framework\App\ActionFlag $actionFlag,
@@ -31,6 +35,7 @@ class LogOutCustomerWhenTokenDoesNotMatch implements \Magento\Framework\Event\Ob
         $this->persistentData = $persistentData;
         $this->customerSession = $customerSession;
         $this->redirect = $redirect;
+        $this->response = $response;
         $this->logger = $logger;
         $this->request = $request;
         $this->actionFlag = $actionFlag;
@@ -38,7 +43,7 @@ class LogOutCustomerWhenTokenDoesNotMatch implements \Magento\Framework\Event\Ob
         $this->generateHashBasedOnCustomerData = $generateHashBasedOnCustomerData;
     }
 
-    public function execute(\Magento\Framework\Event\Observer $observer)
+    public function execute(\Magento\Framework\Event\Observer $observer): void
     {
         if (!$this->persistentData->isEnabled()) {
             return;
@@ -61,23 +66,20 @@ class LogOutCustomerWhenTokenDoesNotMatch implements \Magento\Framework\Event\Ob
         }
 
         try {
-            $this->deleteCustomerToken($this->customerSession->getCustomerId(), $persistentSession);
+            $this->deleteCustomerToken((int) $this->customerSession->getCustomerId(), $persistentSession);
             $this->customerSession->logout();
-
-            $controller = $observer->getControllerAction();
-            $request = $controller->getRequest();
 
             if (!$this->request->isAjax()) {
                 $this->actionFlag->set('', \Magento\Framework\App\ActionInterface::FLAG_NO_DISPATCH, true);
 
-                $this->redirect->redirect($controller->getResponse(), $request->getUriString());
+                $this->redirect->redirect($this->response, $this->request->getUriString());
             }
         } catch (\Exception $e) {
             $this->logger->error(sprintf('Logging out customer when his key is expired failed, reason: %s', $e->getMessage()));
         }
     }
 
-    protected function deleteCustomerToken($customerId, $persistentSession)
+    protected function deleteCustomerToken(int $customerId, \Magento\Persistent\Model\Session $persistentSession): void
     {
         $persistentSessionInDatabase = $persistentSession->loadByCustomerId($customerId);
 
